25d08cd02f5c1ddeb172db7568b85c9dd07505b1
Krzmbrzl <krzmbrzl@gmail.com>
2019-04-26 19:25:19 +0200

    added initial structure

---
+++ addons/OPT/modules.hpp
@@ -0,0 +1,11 @@
+// include the required addons directly from CLib
+#include "\tc\CLib\addons\CLib\ModuleMacros.hpp"
+
+class CfgCLibModules {
+    class PREFIX {
+        path = "OPT\OPT\addons\OPT";
+		
+		dependency[] = {};
+		
+    };
+};
