a43fa694a1a63be60da2b53ae5fbe5a8fd1f8b7f
Krzmbrzl <krzmbrzl@gmail.com>
2019-04-27 17:27:10 +0200

    improved build scripts

--- tools/scripts/buildModOPT.bat
+++ tools/scripts/buildModOPT.bat
@@ -1,18 +1,33 @@
 :: This script will build the OPT mod
 @echo off
 
-set /P version="Which version of the OPT mod do you want to build (dev/release/both - default: dev)? "
+set version=%1
 
-if [%version%] == [] set version=dev
-if [%version%] == [dev] goto dev
-if [%version%] == [stable] set version=release
-if [%version%] == [release] goto release
-if [%version%] == [both] goto release
+if [%version%] == [ask] (
+	set "version="
+	goto :askForVersion
+)
+if [%version%] == [] goto :askForVersion
+::else
+goto processArgs
 
-:: if this code is executed it means that an invalid option has been used
-echo Unknown version %version% - falling back to building dev version%
-set version=dev
-goto dev
+
+:askForVersion
+	:: If this script is being called without arguments, ask what to do
+	set /P version="Which version of the OPT mod do you want to build (dev/release/both - default: dev)? "
+
+
+:processArgs
+	if [%version%] == [] set version=dev
+	if [%version%] == [dev] goto dev
+	if [%version%] == [stable] set version=release
+	if [%version%] == [release] goto release
+	if [%version%] == [both] goto release
+
+	:: if this code is executed it means that an invalid option has been used
+	echo Unknown version %version% - falling back to building dev version%
+	set version=dev
+	goto dev
 
 
 :release
@@ -23,7 +38,7 @@ goto dev
 	
 	..\programs\armake2.exe build -i ..\..\dependencies\CLib\addons\ ..\..\addons\OPT\ ..\..\PBOs\release\@OPT\addons\opt.pbo
 
-if not [%version%] == [both] goto:eof
+if not [%version%] == [both] goto finish
 
 
 :dev
@@ -43,3 +58,11 @@ if not [%version%] == [both] goto:eof
 	del ..\..\addons\OPT\isDev.hpp /q
 	1>NUL copy ..\..\addons\OPT\isDev.hpp.original ..\..\addons\OPT\isDev.hpp
 	del ..\..\addons\OPT\isDev.hpp.original /q
+
+:finish
+	:: if this script hasn't been called from the build-all script and it hasn't been requested not to,
+	:: issue a pause so the user can have a look at the output of the called commands
+	if not [%2] == [buildModAll] (
+		if not [%2] == [noPause] pause
+	)
+	
