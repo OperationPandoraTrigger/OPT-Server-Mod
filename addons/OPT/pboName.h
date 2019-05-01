4df58c2e3522b8b1b61b56ad109f8242dd13f91a
Krzmbrzl <krzmbrzl@gmail.com>
2019-05-01 20:02:34 +0200

    added PBO-name file

---
+++ addons/OPT/pboName.h
@@ -0,0 +1,5 @@
+// Diese Datei muss durch den preprocessor gejagt werden, damit der Name der PBO
+// richtig hier drin steht
+// Da die Mod nachher auf einem Linux-Server laufen soll, muss der PBO-Name kmoplett kleingeschrieben sein
+#include "macros.hpp"
+pboName=QUOTE(opt_v##VERSION)
