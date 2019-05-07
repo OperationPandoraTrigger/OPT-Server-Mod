7b3966151a7d33b79612e9154d3dd0e6855f09d8
GNCLord-MDB <lord.mdb@gmx.net>
2019-05-07 11:18:15 +0200

    Ordner/Dateistruktur erstellt.

--- addons/OPT/modules.hpp
+++ addons/OPT/modules.hpp
@@ -1,11 +1,21 @@
 // include the required addons directly from CLib
 #include "\tc\CLib\addons\CLib\ModuleMacros.hpp"
 
-class CfgCLibModules {
-    class PREFIX {
+class CfgCLibModules 
+{
+    class PREFIX 
+	{
         path = "OPT\OPT\addons\OPT";
 		
 		dependency[] = {};
+		
+		MODULE(ltm) 
+		{
+
+			dependency[] = {"CLib/PerFrame", "Clib/Events"};
+			
+			FNC(clientInit);
+		};
 		
     };
 };
