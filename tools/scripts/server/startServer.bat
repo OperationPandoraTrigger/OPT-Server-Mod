e333b679f8c1ba3103ba8571a3c06a3a2c9871e8
formtapez <github.com@posthub.de>
2019-05-30 16:28:10 +0200

    abort when missing servermods

--- tools/scripts/server/startServer.bat
+++ tools/scripts/server/startServer.bat
@@ -8,12 +8,22 @@ call "%~dp0.\setMetaData.bat"
 
 :: convert relative to absolute path of clib
 :: so armaserver can read it properly
+if not exist "%~dp0..\..\..\PBOs\dev\@CLib" (
+	echo Can't find PBOs\dev\@CLib - did you forget to build CLib before?
+	pause
+	exit 1
+)
 pushd "%~dp0..\..\..\PBOs\dev\@CLib"
 set "clib=%CD%"
 popd
 
 :: convert relative to absolute path of opt
 :: so armaserver can read it properly
+if not exist "%~dp0..\..\..\PBOs\dev\@OPT" (
+	echo Can't find PBOs\dev\@OPT - did you forget to build the OPT-mod before?
+	pause
+	exit 1
+)
 pushd "%~dp0..\..\..\PBOs\dev\@OPT"
 set "opt=%CD%"
 popd
@@ -23,8 +33,8 @@ cd /d "%armaDir%"
 
 :: Copy the server-config into the arma-dir as Arma can only read configs relative
 :: to the exe
+echo Trying to copy config file. This might take a while...
 :copyLoop
-	echo Trying to copy config file. This might take a while...
 	copy /Y "%~dp0.\serverConfig.cfg" "%armaDir%" > NUL 2>&1
 
 	if not [%errorlevel%] == [0]  (
