cf006d86d488748fa9a88c47f54123ce0f4a2426
GNCLord-MDB <lord.mdb@gmx.net>
2019-05-31 19:01:51 +0200

    debug test

--- addons/OPT/macros.hpp
+++ addons/OPT/macros.hpp
@@ -21,9 +21,9 @@
 
 // Do not release with this setting enabled!
 #include "isDev.hpp"
-// #define DEBUGFULL // Enable all debug methods
-// #define ENABLEPERFORMANCECOUNTER // Enable performance counter for function calls
-// #define ENABLEFUNCTIONTRACE // Enable SQF based function tracer
+#define DEBUGFULL // Enable all debug methods
+#define ENABLEPERFORMANCECOUNTER // Enable performance counter for function calls
+#define ENABLEFUNCTIONTRACE // Enable SQF based function tracer
 
 
 #define DISABLECOMPRESSION
