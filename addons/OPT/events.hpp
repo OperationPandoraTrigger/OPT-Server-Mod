ccf80c35e523413d3b5bcc46582022fe249cd478
GNCLord-MDB <lord.mdb@gmx.net>
2019-10-16 14:36:45 +0200

    Events Shop hinzugefügt

--- addons/OPT/events.hpp
+++ addons/OPT/events.hpp
@@ -13,3 +13,21 @@ List all events used by OPT here
 // Unit has been revived
 // params ["_unit"]
 #define EVENT_MEDIC_REVIVED             OPT_EVENT(medic_revived)
+// Laden der Datenmatrix nach öffen des Shop Dialogs
+// params [""]
+#define EVENT_SHOP_KAUF_ONLOAD         OPT_EVENT(shop_kauf_onload)
+// Erneuerung des Infosfeld nach änderung der Auswahl 
+// params ["_typ"]
+#define EVENT_SHOP_INFOFELD        OPT_EVENT(shop_infofeld)
+// Kauf Shop ausführen 
+// params ["_typ"]
+#define EVENT_SHOP_ORDER      OPT_EVENT(shop_order)
+// Laden der Datenmatrix nach öffen des Konfiguration Dialogs
+// params ["_typ"]
+#define EVENT_SHOP_KONFIG_ONLOAD         OPT_EVENT(shop_konfig_onload)
+// Erneuerung des Infosfeld nach änderung der Auswahl 
+// params [""]
+#define EVENT_SHOP_NEUBERECHNUNG        OPT_EVENT(shop_neuberechnung)
+// Kauf in Konfigurator ausführen 
+// params ["_typ"]
+#define EVENT_SHOP_KONFIG_ORDER      OPT_EVENT(shop_konfig_order)
