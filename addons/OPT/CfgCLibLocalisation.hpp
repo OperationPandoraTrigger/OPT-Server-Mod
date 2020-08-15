0cfb1d895fbeb9ce4999c9ce5cd835cbbf75e21a
maxSzone <33429754+maxSzone@users.noreply.github.com>
2020-08-15 10:19:22 +0200

    Add Strings for Sectorcontrol

--- addons/OPT/CfgCLibLocalisation.hpp
+++ addons/OPT/CfgCLibLocalisation.hpp
@@ -17,6 +17,50 @@ class CfgCLibLocalisation {
 				English = "Choice of the attack flag";
 				German = "Wahl der Angriffsflagge";
 			};
+			class SECTORCONTROL_INSTRUCTION {
+				English = "All possible attack flags are marked with a black circle. The selected flag is highlighted in blue, green or red, depending on the side. It can be selected as often as you like.";
+				German = "Alle möglichen Angriffsflaggen sind mit einem schwarzen Kreis markiert. Die gewählte Flagge wird je nach Seite blau,grün oder rot hervorgehoben. Es kann beliebig oft neu gewählt werden.";
+			};
+			class SECTORCONTROL_INSTRUCTION_HEADER {
+				English = "Instructions";
+				German = "Instruktionen";
+			};
+			class FLAG_CONFIRM {
+				English = "%1 of %2 allowed flags selected.";
+				German = "%1 von %2 erlaubten Flaggen gewählt.";
+			};
+			class MINE_VIOLATION {
+				English = "Mine laid in the mine-restricted area! Mine was deleted.";
+				German = "Mine in der Minensperrzone gelegt! Mine wurde gelöscht.";
+			};
+			class MINE_VIOLATION_HEADER {
+				English = "Restricted area";
+				German = "Sperrzone";
+			};
+			class POINT_NO_DOMINATOR_AAFvCSAT {
+				English = "No Dominator (AAF %1 | CSAT %2)";
+				German = "Kein Dominator (AAF %1 | CSAT %2)";
+			};
+			class POINT_NO_DOMINATOR_NATOvCSAT {
+				English = "No Dominator (NATO %1 | CSAT %2)";
+				German = "Kein Dominator (NATO %1 | CSAT %2)";
+			};
+			class POINT_NO_DOMINATOR_NATOvAAF {
+				English = "No Dominator (NATO %1 | AAF %2)";
+				German = "Kein Dominator (NATO %1 | AAF %2)";
+			};
+			class CAPTURE_FLAG {
+				English = "Capture the flag";
+				German = "Flagge ziehen";
+			};
+			class TELEPORT_MSG_HEADER {
+				English = "Teleport";
+				German = "Teleport";
+			};
+			class TELEPORT_MSG {
+				English = "Select position to teleport with left click.";
+				German = "Position zum Teleport mit Linksklick auswählen.";
+			};
 		};
 	};
 };
