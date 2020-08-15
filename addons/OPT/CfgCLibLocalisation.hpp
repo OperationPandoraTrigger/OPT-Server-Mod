a5ec348e844ece73d43353e27835d881350fa6b7
maxSzone <33429754+maxSzone@users.noreply.github.com>
2020-08-15 09:32:53 +0200

    Rebuild Strings from Strigtable

---
+++ addons/OPT/CfgCLibLocalisation.hpp
@@ -0,0 +1,22 @@
+class CfgCLibLocalisation {
+
+	supportedLanguages[] = {"English", "German"};
+	class OPT{
+
+		class SECTORCONTROL{
+			
+			class ATTACK_FLAG {
+				English = "Attack flag";
+				German = "Angriffsflagge";
+			};
+			class FLAG_INST {
+				English = "Please select a flag via the mousewheel menu. This must be done during the ceasefire!";
+				German = "Bitte eine Flagge über das Mausradmenü wählen. Dies muss während der Waffenruhe geschehen!";
+			};
+			class FLAG_CHOOSE {
+				English = "Choice of the attack flag";
+				German = "Wahl der Angriffsflagge";
+			};
+		};
+	};
+};
