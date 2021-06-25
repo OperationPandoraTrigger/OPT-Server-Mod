ffc6762a70595f377a23f9ad174da4ae76cd7745
Florian Kinder <florian.kinder@fankserver.com>
2021-06-25 12:25:17 +0200

    Improved Zeus handling to cleanup obsolete module

---
+++ addons/main/ZEUS/fn_clientInit.sqf
@@ -0,0 +1,24 @@
+/**
+* Author: Fank
+* Creates Zeus for client
+*
+* Arguments:
+* None
+*
+* Return Value:
+* None
+*
+*/
+#include "macros.hpp"
+
+if (!OPT_SECTORCONTROL_trainingon) exitWith {};
+
+["missionStarted",
+{
+	[ZEUS_SWAP_EVENT, [player]] call CFUNC(serverEvent);
+
+	player addEventHandler ["Respawn", {
+		params ["_unit","_corpse"];
+		[ZEUS_SWAP_EVENT, [_unit, _corpse]] call CFUNC(serverEvent);
+	}];
+}] call CFUNC(addEventhandler);
--- addons/main/modules.hpp
+++ addons/main/modules.hpp
@@ -13,15 +13,15 @@ class CfgCLibModules
             FNC(initCBASettings);
             FNC(clientInit);
             FNC(gps);
-        }; 
+        };
 
-        MODULE(SHOP) 
+        MODULE(SHOP)
         {
             dependency[] = {"CLib/PerFrame", "Clib/Events"};
             FNC(clientInit);
             FNC(initCBASettings);
             FNC(config);
-            FNC(einlesenInShopDialog);    
+            FNC(einlesenInShopDialog);
             FNC(fillLB);
             FNC(getvehicleinfo);
             FNC(getprice);
@@ -49,7 +49,7 @@ class CfgCLibModules
             FNC(Init);
         };
 
-        MODULE(REVIVE) 
+        MODULE(REVIVE)
         {
             dependency[] = {"CLib/PerFrame", "Clib/Events", "Clib/StateMachine", "Clib/MapGraphics", "Clib/RemoteExecution"};
             FNC(clientInit);
@@ -66,7 +66,7 @@ class CfgCLibModules
             FNC(clientInitcbaclassevents);
         };
 
-        MODULE(GELDZEIT) 
+        MODULE(GELDZEIT)
         {
             dependency[] = {"CLib/PerFrame", "Clib/Events", "Clib/StateMachine", "Clib/MapGraphics", "Clib/RemoteExecution"};
             FNC(clientInit);
@@ -81,14 +81,12 @@ class CfgCLibModules
             FNC(updateplayerlist);
             FNC(earplugs);
             FNC(briefing);
-            FNC(Zeus);
-            FNC(init);
             FNC(beam);
             FNC(haftladungen);
             FNC(clientInitcbaclassevents);
         };
 
-        MODULE(SECTORCONTROL) 
+        MODULE(SECTORCONTROL)
         {
             dependency[] = {"CLib/PerFrame", "Clib/Events", "Clib/StateMachine", "Clib/MapGraphics", "Clib/RemoteExecution"};
             FNC(clientInit);
@@ -111,7 +109,7 @@ class CfgCLibModules
             FNC(drawline);
         };
 
-        MODULE(FIELDREPAIR) 
+        MODULE(FIELDREPAIR)
         {
             dependency[] = {"CLib/PerFrame", "Clib/Events", "Clib/StateMachine", "Clib/MapGraphics", "Clib/RemoteExecution"};
             FNC(clientInit);
@@ -132,14 +130,14 @@ class CfgCLibModules
             FNC(checkRepairs);
         };
 
-        MODULE(HUD) 
+        MODULE(HUD)
         {
             dependency[] = {"CLib/PerFrame", "Clib/Events", "Clib/StateMachine", "Clib/MapGraphics", "Clib/RemoteExecution"};
             FNC(clientInit);
             FNC(updatehud);
         };
 
-        MODULE(CARGO) 
+        MODULE(CARGO)
         {
             dependency[] = {"CLib/PerFrame", "Clib/Events", "Clib/StateMachine", "Clib/MapGraphics", "Clib/RemoteExecution"};
             FNC(clientInit);
@@ -152,7 +150,7 @@ class CfgCLibModules
             FNC(setupcargospaceandsize);
         };
 
-        MODULE(RULES) 
+        MODULE(RULES)
         {
             dependency[] = {"CLib/PerFrame", "Clib/Events", "Clib/StateMachine", "Clib/MapGraphics", "Clib/RemoteExecution"};
             FNC(clientInit);
@@ -169,6 +167,13 @@ class CfgCLibModules
         MODULE(WEATHERTIME)
         {
             FNC(initCBASettings);
+            FNC(serverInit);
+        };
+
+        MODULE(ZEUS)
+        {
+            dependency[] = {"Clib/Events"};
+            FNC(clientInit);
             FNC(serverInit);
         };
     };
